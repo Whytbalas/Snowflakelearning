4b3f6a771830dc7655599723c052b40341e8453c
Whytbalas <105111575+Whytbalas@users.noreply.github.com>
2022-05-10 15:30:51 +0530

    Update streamlit_app.py
    Additional menu's added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 streamlit.title("My Parents New Healthy diner")
 streamlit.header("Breakfast Favourties")
 streamlit.text("🥣Omega 3 & Blueberry Oatmeal")
@@ -6,3 +7,5 @@ streamlit.text("🥗Kale, Spinach and Rocket Smoothie")
 streamlit.text(" 🐔Hard-Boiled Free-Range Egg")
 streamlit.text("🥑🍞 Avacoda Toast")
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+myfruit_list=pandas.read.csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(myfruit_list)
