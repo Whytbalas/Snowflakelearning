06d5f1c8e37c93a39ff237177a24ec8a704a22c9
Whytbalas <105111575+Whytbalas@users.noreply.github.com>
2022-05-10 15:24:42 +0530

    Update streamlit_app.py
    Changed title and include few emojiis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 streamlit.title("My Parents New Healthy diner")
-streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 & Blueberry Oatmeal")
-streamlit.text("Kale, Spinach and Rocket Smoothie")
-streamlit.text("Hard-Boiled Free-Range Egg")
+streamlit.header("Breakfast Favourties")
+streamlit.text("🥣Omega 3 & Blueberry Oatmeal")
+streamlit.text("🥗Kale, Spinach and Rocket Smoothie")
+streamlit.text(" 🐔Hard-Boiled Free-Range Egg")
+streamlit.text("🥑🍞 Avacoda Toast")
                
